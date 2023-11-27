dcef8fcc0787c8143b3aad0f12ef8189bb4edaae
oiuk <112466621+oiuk@users.noreply.github.com>
2023-11-27 16:23:04 +0900

    Add files via upload

---
+++ test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+// test
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'hi'
+        ),
+      ),
+    );
+  }
+}
+
