abc09b5015a13e3efa56fd340975e0fa32442967
oiuk <112466621+oiuk@users.noreply.github.com>
2023-11-27 15:25:52 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:post_and_comment/screen/Post_Screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: PostScreen(),
+    ),
+  );
+}
